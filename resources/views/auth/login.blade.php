8ebc79fdd5e7f087984981efb1bd1f6229ffd947
BENJAH <benjahjp@gmail.com>
2022-09-19 12:11:58 +0300

    login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,15 +64,9 @@
 
                                 <div class="row mb-3">
                                     <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary btn-block btn-user rounded-pill">
+                                        <button type="submit" class="col-md-8 btn btn-primary btn-block btn-user rounded-pill">
                                             {{ __('Login') }}
                                         </button>
-
-                                        <!-- @if (Route::has('password.request'))
-                                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                {{ __('Forgot Your Password?') }}
-                                            </a>
-                                        @endif -->
                                     </div>
                                 </div>
                             </form>
